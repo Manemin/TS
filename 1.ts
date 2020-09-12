4758a7e2ccc0aa380fe3532d38676771772be611
Manemin <manemin80@gmail.com>
2020-09-13 00:15:58 +0300

    add class deputy

---
+++ 1.ts
@@ -0,0 +1,61 @@
+class Human {
+    weight: number
+    height: number
+    constructor( weight, height) {
+        this.weight = weight;
+        this.height = height;
+    }
+}
+
+class Deputy extends Human {
+    name: string;
+    surname: string;
+    age: number;
+    bribeTaker?: boolean;
+    bribe: number;
+    constructor(weight, height, name, surname, age, bribeTaker, bribe = 0 ) {
+        super(weight, height);
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.bribeTaker = bribeTaker;
+        this.bribe = bribe;
+    }
+    toGiveBribe(sum: number) {
+        if (this.bribeTaker === true) {
+            if (sum > 20000) {
+                Math.round(Math.random()) ? this.bribe += sum : console.log('No thanks');
+            }
+        } else {
+            console.log('I\'m calling the Police!!!');
+        }
+    }
+}
+
+class Fraction {
+    list: Object[] = [];
+
+    addDeputy(deputy) {
+        this.list.push(deputy);
+    }
+
+    delDeputy(name) {
+        const i = this.list.indexOf(name);
+        (i >= 0) ? this.list.splice(i, 1) : console.log('Not present');
+    }
+}
+
+class Senate {
+    group: Object[] = [];
+    addGroup(name) {
+        this.group.push(name);
+    }
+    delGroup(name) {
+        const i = this.group.indexOf(name);
+        (i >= 0) ? this.group.splice(i, 1) : console.log('Not present');
+    }
+    printAllGroup() {
+        // this.group.forEach(el => console.log(el));
+        console.log(this.group);
+    }
+}
